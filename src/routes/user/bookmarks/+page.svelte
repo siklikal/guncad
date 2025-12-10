edab1f94ee1cadd85a66019dfc6abc137baa657c
siklikal <siklikal@protonmail.com>
2025-12-10 19:54:57 +0700

    general updates

---
+++ src/routes/user/bookmarks/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Fa from 'svelte-fa';
+	import {
+		faEye,
+		faHeart,
+		faGem,
+		faStar,
+		faFire,
+		faBookmark,
+		faShare,
+		faDownload
+	} from '@fortawesome/free-solid-svg-icons';
+	import { getTagColorClass } from '$lib/utils/tagColors';
+	import { Button } from '$lib/components/ui/button/index.js';
+</script>
+
+<div class="container mx-auto">
+	<h1 class="text-4xl font-semibold">Bookmarks</h1>
+</div>
